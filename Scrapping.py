50bc1e8b11169c41597f300cb4cf1cbd3b63d53d
ljatsa <jatsasteve@gmail.com>
2023-02-27 16:34:05 +0100

    Update 27.02.2023

--- Scrapping.py
+++ Scrapping.py
@@ -3,23 +3,28 @@ import pprint
 import requests
 import re
 
-#url = "https://fr.wikipedia.org/wiki/Twitter"
-url = "https://www.eurosport.fr/football/"
-
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-adresse = []
-string_r = r"^(https://)([A-Za-z0-9][-.%_/#]*)*/?$"
-string_e = re.compile(string_r)
+url = "https://fr.wikipedia.org/wiki/Twitter"
+# url = "https://www.eurosport.fr/football/"
 
 
-links = soup.find_all(name="a")
-for link in links:
-    var_link = link.get("href")
-    if var_link:
-        if string_e.search(var_link ):
-            adresse.append(var_link )
 
-for r_link in adresse:
-    print(r_link)
 
+
+def scrapping_link(url:str)-> list[str]:
+    response = requests.get(url)
+    if response.status_code != 200:
+        return []
+    
+    soup = BeautifulSoup(response.text, "html.parser")
+    adresse = []
+    string_r = r"^(https://)([A-Za-z0-9][-.%_/#]*)*/?$"
+    string_e = re.compile(string_r)
+
+
+    links = soup.find_all(name="a")
+    for link in links:
+        if var_link := link.get("href"):
+            if string_e.search(var_link):
+                adresse.append(var_link)
+
+    return adresse
--- scrapping_worter.py
+++ scrapping_worter.py
@@ -4,27 +4,35 @@ import requests
 import re
 
 
-url = "https://fr.wikipedia.org/wiki/Twitter"
-woerter = []
-row_worter = []
-elements = ["h1", "h2", "h3", "h4", "h5", "h6", "p", "span", "a"]
-character = ["/", "\\", ":", ".", ",", "#", "[", "]", "(", ")"]
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-for el in elements:
-    wort = [wo.getText() for wo in soup.find_all(name=el)]
-    row_worter.extend(wort)
+#
 
-for w in row_worter:
-    woerter.extend(w.replace("/", " ").split())
+def scrapping_word(url: str) -> list[str]:
+    woerter = []
+    row_worter = []
+    correct = []
 
-correct = []
-for letter in woerter:
-    for ch in character:
-        if ch in letter:
-            print(f"je suis le caractere: {ch}")
-            print(f"je suis la: {letter}")
-            " ".join(letter.split().remove(ch))
+    elements = ["h1", "h2", "h3", "h4", "h5", "h6", "p", "span", "a"]
+    character = ["/", "\\", ":", ".", ",", ";", "#", "[", "]", "(", ")", "'", '"', "!", "?", "*", "+", "-", "_", "|",
+                 "“", "”", "«", "»", ">", "<", "°", "^", "$", "§", "%", "&", "{", "}", "=", "~", "€", "@", "µ", "…",
+                 "„", "י", "יִ", "ד", "ᱤ"]
+
+    response = requests.get(url)
+    if response.status_code != 200:
+        return []
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    for el in elements:
+        wort = [wo.getText() for wo in soup.find_all(name=el)]
+        row_worter.extend(wort)
+
+    for w in row_worter:
+        woerter.extend(w.split())
+
+    for letter in woerter:
+        if 3 < len(letter) < 15:
+            for ch in character:
+                letter = letter.replace(ch, "")
             correct.append(letter)
-    print(letter)
-print(correct)
+
+
+    return correct
